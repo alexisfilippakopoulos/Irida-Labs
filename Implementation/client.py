ce653542dec978c4d9b00585f1410bd6dd0df68b
alex filipak <alexisfilippakopoulos1@gmail.com>
2023-12-21 16:38:48 +0200

    TODO Aggregation

--- Implementation/client.py
+++ Implementation/client.py
@@ -4,15 +4,21 @@ import pickle
 import sys
 import torch.nn as nn
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset, Subset, random_split
 import torch
+import time
 
 # Events to ensure synchronization
 start_label_event = threading.Event()
 labels_recvd_event = threading.Event()
+initial_weights_event = threading.Event()
+start_training_event = threading.Event()
 
 #Constants
 BATCH_SIZE = 64
+LEARNING_RATE = 1e-2
+GLOBAL_EPOCHS = 10
+VALIDATION_SPLIT = 0.2
 
 class Client:
     def __init__(self, server_ip, server_port, client_ip, client_port):
@@ -23,12 +29,12 @@ class Client:
         self.client_model = ClientModel()
         self.classifier_model = ClientClassifier()
         self.server_labels = []
-        self.event_dict = {'LABELS': labels_recvd_event, 'LABEL_EVENT': start_label_event}
+        self.event_dict = {'LABELS': labels_recvd_event, 'LABEL_EVENT': start_label_event, 'INITIAL_WEIGHTS': initial_weights_event, 'TRAIN': start_training_event}
         self.device = self.get_device()
         self.criterion = nn.CrossEntropyLoss()
-        self.model_optimizer = torch.optim.SGD(params=self.client_model.parameters(), lr=0.01)
-        self.classifier_optimizer = torch.optim.SGD(params=self.classifier_model.parameters(), lr=0.01)
-        self.data_features = []
+        self.model_optimizer = torch.optim.SGD(params=self.client_model.parameters(), lr=LEARNING_RATE)
+        self.true_labs = []
+        self.classifier_optimizer = torch.optim.SGD(params=self.classifier_model.parameters(), lr=LEARNING_RATE)
         print(f'Using {self.device}')
 
     def create_socket(self):
@@ -62,7 +68,6 @@ class Client:
         except socket.error as error:
             print(f'Error receiving data:\n{error}')
 
-
     def send_packet(self, data):
         """
         Packs and sends a payload of data to the server.
@@ -79,26 +84,44 @@ class Client:
         data = pickle.loads(data)
         header = list(data.keys())[0]
         if header == 'LABELS':
-            self.server_labels.append(data['LABELS'])
-            self.event_dict['LABELS'].set()
-        elif header == 'LABEL_EVENT':
-            start_label_event.set()
-            print('[+] Started training with server')
-            
+            self.server_labels.append(data[header])
+        elif header == 'INITIAL_WEIGHTS':
+            self.client_model.load_state_dict(data[header][0])
+            self.classifier_model.load_state_dict(data[header][1]) 
+            print('[+] Loaded initial weights successfully')
+            self.send_packet(data={'OK': b''})
+        self.event_dict[header].set()
 
-    def get_dataset(self):
+    def get_dataset(self, shard_id):
         """
-        Downloads CIFAR10 dataset.
+        Downloads FashionMNIST dataset. Creates a Subset of the data
         Returns:
-            training_data: Training dataset
-            testing_data: Testing dataset
+            train_subset: Training dataset
+            test_subset: Testing dataset
         """
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        training_data = datasets.CIFAR10(download=True, root='/data', train=True, transform=transform)
-        testing_data = datasets.CIFAR10(download=True, root='/data', train=False, transform=transform)
-        return training_data, testing_data
+        shards = {'0': [0, 1], '1': [2, 3], '2': [4, 5], '3': [6, 7], '4': [8, 9]}
+        training_indices = []
+        testing_indices = []
+
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, ), (0.5, ), )])
+        training_data = datasets.FashionMNIST(download=True, root='/data', train=True, transform=transform)
+        testing_data = datasets.FashionMNIST(download=True, root='/data', train=False, transform=transform)
+        
+        for i in range(len(training_data.targets)):
+            if training_data.targets[i] in shards[shard_id]:
+                training_indices.append(i)
+
+        for i in range(len(testing_data.targets)):
+            if testing_data.targets[i] in shards[shard_id]:
+                testing_indices.append(i)
+
+        train_subset = Subset(dataset=training_data, indices=training_indices)
+        test_subset = Subset(dataset=testing_data, indices=testing_indices)
+
+        print(f'Training: {len(train_subset)}, Testing: {len(test_subset)}')
+        return train_subset, test_subset
     
-    def get_dataloader(self, data: datasets, batch_size: int):
+    def get_dataloader(self, data: datasets, batch_size: int, shuffle: bool, split_flag: bool = False):
         """
         Creates a DataLoader object for a specified dataset.
         Args:
@@ -106,49 +129,75 @@ class Client:
         Returns:
             DataLoader for the specidied dataset.
         """
-        return DataLoader(dataset=data, batch_size=batch_size, num_workers=2, pin_memory=True)
+        if split_flag:
+            training_data, validation_data = random_split(data, [int((1 - VALIDATION_SPLIT) * len(data)), int(VALIDATION_SPLIT * len(data))])
+            return DataLoader(dataset=training_data, batch_size=batch_size, num_workers=2, pin_memory=True, shuffle=shuffle), DataLoader(dataset=validation_data, batch_size=batch_size, num_workers=2, pin_memory=True, shuffle=shuffle)
+        
+        return DataLoader(dataset=data, batch_size=batch_size, num_workers=2, pin_memory=True, shuffle=shuffle)
 
     def get_device(self):
         return torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     def get_labels(self, train_dl: DataLoader):
+        print('[+] Waiting for labeling process with server')
         start_label_event.wait()
         start_label_event.clear()
+        print('[+] Starting labeling process with server')
         with torch.inference_mode():
             for i, (inputs, labels) in enumerate(train_dl):
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
+                self.true_labs.append(labels)
                 split_features_maps = self.client_model(inputs)
-                self.send_packet(data={'model_outputs': split_features_maps})
+                #if i == len(train_dl) - 1:
+                if i == 1:
+                    self.send_packet(data={'final_model_outputs': split_features_maps})
+                    labels_recvd_event.wait()
+                    break
+                else:
+                    self.send_packet(data={'model_outputs': split_features_maps})
                 labels_recvd_event.wait()
                 labels_recvd_event.clear()
-        self.send_packet(data={'FINISH': b''})
-        print('[+] Gathered labels')
-        print(len(torch.cat(self.server_labels, 0)))
+        print('[+] Finished labeling')
+        self.server_labels = torch.cat(tensors=self.server_labels, dim=0)
+        self.true_labs = torch.cat(tensors=self.true_labs, dim=0)
 
-    def train_one_epoch(self, train_dl):
+    def train_one_epoch(self, train_dl: DataLoader):
         self.client_model.train(), self.classifier_model.train()
+        curr_loss = 0.
         for i, (inputs, labels) in enumerate(train_dl):
             self.model_optimizer.zero_grad(), self.classifier_optimizer.zero_grad()
             inputs, labels = inputs.to(self.device), labels.to(self.device)
             outputs = self.classifier_model(self.client_model(inputs))
             loss = self.criterion(outputs, labels)
             loss.backward()
+            curr_loss += loss.item()
             self.model_optimizer.step(), self.classifier_optimizer.step()
-               
+        print(f'\t[+] Average Training Loss: {(curr_loss / len(train_dl)): .2f}')
 
-class ClientClassifier(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(in_features=5*5*256, out_features=10)
-        
-    def forward(self, x):
-        x = self.fc1(torch.flatten(x, 1))
-        return x
+    def validate(self, val_dl: DataLoader):
+        self.client_model.eval(), self.classifier_model.eval()
+        curr_vloss = 0.
+        corr = 0
+        total = 0
+        for i, (inputs, labels) in enumerate(val_dl):
+            inputs, labels = inputs.to(self.device), labels.to(self.device)
+            outputs = self.classifier_model(self.client_model(inputs))
+            vloss = self.criterion(outputs, labels)
+            curr_vloss += vloss.item()
+            _, preds = torch.max(outputs, dim=1)
+            corr += (preds == labels).sum().item()
+            total += labels.size(0)
+        del inputs, labels, outputs
+        avg_vloss = curr_vloss / len(val_dl)
+        val_acc = corr / total
+        print(f'\t[+] Average Validation Loss: {avg_vloss: .2f}\n\t[+] Average Validation Accuracy: {val_acc: .2%}')
+
+               
 
 class ClientModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
         self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
@@ -161,22 +210,62 @@ class ClientModel(nn.Module):
         x = self.relu(self.conv3(x))
         x = self.pool(self.relu(self.conv4(x)))
         return x
+
+class ClientClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features=4*4*256, out_features=10)
+        
+    def forward(self, x):
+        x = self.fc1(torch.flatten(x, 1))
+        return x
+    
+class CustomDataset(Dataset):
+    def __init__(self, original_data, server_labels):
+        self.data = original_data
+        self.labels = server_labels
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        img, _ = self.data[index]
+        label = self.labels[index]
+        return img, label
     
 
 if __name__ == '__main__':
 # To execute, server_ip, server_port and client_ip, client_port must be specified from the cl.
-    if len(sys.argv) != 5:
-        print('Incorrect number of command-line arguments\nTo execute, server_ip, server_port, client_ip and client_port must be specified from the cl.')
+    if len(sys.argv) != 6:
+        print('Incorrect number of command-line arguments\nTo execute, server_ip, server_port, client_ip, client_port and shard_id must be specified from the cl.')
         sys.exit(1)
 
     client = Client(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
     client.create_socket()
     threading.Thread(target=client.listen_for_messages).start()
-    training_data, testing_data = client.get_dataset()
-    train_dl, test_dl = client.get_dataloader(data=training_data, batch_size=BATCH_SIZE), client.get_dataloader(data=testing_data, batch_size=BATCH_SIZE)
-    #client.train(train_dl=train_dl)
-    print('Data: ', len(training_data))
-    print('DL: ', len(train_dl))
+    training_data, testing_data = client.get_dataset(shard_id=sys.argv[5])
+    train_dl, val_dl = client.get_dataloader(data=training_data, batch_size=BATCH_SIZE, shuffle=False, split_flag=True)
+    test_dl =  client.get_dataloader(data=testing_data, batch_size=BATCH_SIZE, shuffle=False)
+    
+    start = time.time()
     client.get_labels(train_dl=train_dl)
-    print(len(client.server_labels))
+    end = time.time()
+    print('Time to label: ', end - start)
+    # Custom dataset with server preds
+    """training_data = CustomDataset(training_data, client.server_labels)
+    train_dl = client.get_dataloader(data=training_data, batch_size=BATCH_SIZE, shuffle=True)
+    print('[+] Custom dataset successfully created')
+    print(len(training_data))
+    print(len(train_dl))"""
+    for e in range(GLOBAL_EPOCHS):
+        print('[+] Waiting for training signal')
+        start_training_event.wait()
+        start_training_event.clear()
+        print(f'[+] Started training for global epoch: {e}')
+        client.train_one_epoch(train_dl=train_dl)
+        client.validate(val_dl=val_dl)
+        client.send_packet(data={'UPDATED_WEIGHTS': [client.client_model.state_dict(), client.classifier_model.state_dict()]})
+        print(f'[+] Waiting for aggregated global model')
+
+    
 
--- Implementation/server.py
+++ Implementation/server.py
@@ -9,24 +9,31 @@ import torch
 # Events to ensure synchronization
 features_recvd_event = threading.Event()
 sufficient_clients_event = threading.Event()
-epoch_finished = threading.Event()
+labeling_finished = threading.Event()
+label_lock = threading.Lock()
+client_recvd_event = threading.Event()
 
 # Constants
-MIN_PARTICIPANTS = 1
+MIN_PARTICIPANTS = 2
+GLOBAL_EPOCHS = 10
 
 class Server:
     def __init__(self, server_ip, server_port):
         self.connected_clients = {}
         self.trained_clients = []
+        self.labeled_clients = []
         self.ip = server_ip
         self.port = int(server_port)
         self.server_db = 'Implementation/server_data/server_db.db'
+        torch.manual_seed(32)
         self.server_model = ServerModel()
-        self.event_dict = {'model_outputs': features_recvd_event, 'enough_clients': sufficient_clients_event, 'FINISH': epoch_finished}
+        self.event_dict = {'model_outputs': features_recvd_event, 'OK': client_recvd_event, 'final_model_outputs': labeling_finished}
         self.device = self.get_device()
         print(f'Using {self.device}')
-        #self.client_model = ClientModel()
-        #self.classifier_model = ClientClassifier()
+        torch.manual_seed(32)
+        self.client_model = ClientModel()
+        torch.manual_seed(32)
+        self.classifier_model = ClientClassifier()
 
     def create_db_schema(self):
         """
@@ -108,6 +115,7 @@ class Server:
                 client_socket, client_address = self.server_socket.accept()
                 client_id = self.handle_connections(client_address, client_socket)
                 threading.Thread(target=server.listen_for_messages, args=(client_socket, client_id)).start()
+                threading.Thread(target=self.give_labels, args=(client_id, client_socket)).start()
         except socket.error as error:
             print(f'Connection handling thread failed:\n{error}')
             
@@ -123,18 +131,18 @@ class Server:
                 data_chunk = client_socket.recv(4096)
                 data_packet += data_chunk
                 if (b'<END>'in data_packet) and (b'<START>' in data_packet):
-                        #data = data_packet.split(b'<START>')[1].split(b'<END>')[0]
-                        threading.Thread(target=self.store_data, args=(data_packet, client_id)).start()
+                        threading.Thread(target=self.handle_data, args=(data_packet, client_id)).start()
                         data_packet = b'' 
                 if not data_chunk:
                     break
         except socket.error as error:
-            print(f'Error receiving data from {client_socket}:\n{error}')
+            print(f'Error receiving data from {client_id, self.connected_clients[client_id][0]}:\n{error}')
             client_socket.close()
+            self.connected_clients.pop(client_id)
     
     def handle_connections(self, client_address: tuple, client_socket: socket.socket):
         """
-        When a client connects -> Add him on db if nonexistent and append to connected_clients list
+        When a client connects -> Add him on db if nonexistent, append to connected_clients list and trasmit initial weights
         Args: Tuple (client_ip, client_port)
         """
         client_ip, client_port = client_address
@@ -157,10 +165,12 @@ class Server:
         else:
             client_id = exists[0][0]
         self.connected_clients[client_id] = (client_address, client_socket)
-        print(f'[+] Client {client_address} connected\n[+] Connected clients: {len(self.connected_clients)}')
+        print(f'[+] Client {client_id, client_address} connected -> Connected clients: {len(self.connected_clients)}')
+        server.send_packet(data={'INITIAL_WEIGHTS': [server.client_model.state_dict(), server.classifier_model.state_dict()]}, client_socket=client_socket)
+        print(f'[+] Transmitted initial weights to client {client_id, client_address}')
         return client_id
 
-    def send_packet(self, data, client_socket: socket.socket):
+    def send_packet(self, data: dict, client_socket: socket.socket):
         """
         Packs and sends a payload of data to a specified client.
         Args:
@@ -172,47 +182,79 @@ class Server:
         except socket.error as error:
             print(f'Message sending failed with error:\n{error}')
 
-    def store_data(self, data, client_id):
+    def handle_data(self, data: dict, client_id: int):
+        """
+        
+        """
         data = pickle.loads(data.split(b'<START>')[1].split(b'<END>')[0])
         header = list(data.keys())[0]
-        if header != 'FINISH':
+        if (header.__contains__('model_outputs')):
             query = f"""
-            INSERT INTO training (client_id, {header}) VALUES (?, ?)
+            INSERT INTO training (client_id, model_outputs) VALUES (?, ?)
             ON CONFLICT (client_id) DO
-            UPDATE SET {header} = ?
+            UPDATE SET model_outputs = ?
             """
             serialized_data = pickle.dumps(data[header])
             self.execute_query(query=query, values=(client_id, serialized_data, serialized_data))
-            self.event_dict[header].set()
-        else:
-            self.event_dict[header].set()
-            print('Fin event', epoch_finished.is_set())
+            self.event_dict['model_outputs'].set() if header == 'final_model_outputs' else None
+        elif header == 'UPDATED_WEIGHTS':
+            query = """
+            INSERT INTO training (client_id, model_outputs) VALUES (?, ?)
+            ON CONFLICT (client_id) DO
+            UPDATE SET updated_weights = ? """
+            serialized_data = pickle.dumps(data[header])
+            self.execute_query(query=query, values=(client_id, serialized_data, serialized_data))
+            self.trained_clients.append(client_id)
+            print(f"[+] Received updated weights of client: {client_id, self.connected_clients[client_id][0]}")
+        elif header == 'OK':
+            pass
+        self.event_dict[header].set() if header in self.event_dict.keys() else None
 
-    def transmit_initial_weights(self):
+
+    def initialize_models(self):
         pass
-    
+        
     def get_device(self):
+        """
+        Check available devices (cuda or cpu). If cuda then cuda else cpu
+        Returns: A torch.device() Object
+        """
         return torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     
-    def give_labels(self, client_id):
-        # Wait client's feature maps
-        while True:
-            if epoch_finished.is_set() == True:
-                break
-            features_recvd_event.wait()
-            features_recvd_event.clear()
-            query = 'SELECT model_outputs FROM training WHERE client_id = ?'
-            client_features = pickle.loads(self.execute_query(query=query, values=(client_id, ), fetch_data_flag=True))
-            server_outputs = self.server_model(client_features)
-            _, preds = torch.max(server_outputs, 1) 
-            self.send_packet(data={'LABELS': preds}, client_socket=self.connected_clients[client_id][1])
-        print('Finished training')
+    def give_labels(self, client_id: int, client_socket: socket.socket):
+        """
+        Labeling process. Wait until features arrive, infer from them, send predictions until all data is labeled.
+        Args:
+            client_id: The client identifier
+            client_socket: The client's socket
+        """
+        with label_lock:
+            client_recvd_event.wait()
+            client_recvd_event.clear()
+            print(f'[+] Labeling with client {client_id, self.connected_clients[client_id][0]}')
+            server.send_packet(data={'LABEL_EVENT': b''}, client_socket=client_socket)
+            with torch.inference_mode():
+                while True:
+                    features_recvd_event.wait()
+                    features_recvd_event.clear()
+                    query = 'SELECT model_outputs FROM training WHERE client_id = ?'
+                    client_features = pickle.loads(self.execute_query(query=query, values=(client_id, ), fetch_data_flag=True))
+                    server_outputs = self.server_model(client_features)
+                    _, preds = torch.max(server_outputs, 1) 
+                    self.send_packet(data={'LABELS': preds}, client_socket=self.connected_clients[client_id][1])
+                    if labeling_finished.is_set():
+                        labeling_finished.clear()
+                        break
+            print(f'[+] Finished labeling with client {client_id, self.connected_clients[client_id][0]}')
+            self.labeled_clients.append(client_id)
 
+    def aggregate_global_model(self):
+        pass
 
 class ClientModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
         self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
@@ -229,7 +271,7 @@ class ClientModel(nn.Module):
 class ClientClassifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(in_features=5*5*256, out_features=10)
+        self.fc1 = nn.Linear(in_features=4*4*256, out_features=10)
         
     def forward(self, x):
         x = self.fc1(torch.flatten(x, 1))
@@ -239,7 +281,7 @@ class ServerModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv5 = nn.Conv2d(256, 512, kernel_size=3)
-        self.fc1 = nn.Linear(3*3*512, 256)
+        self.fc1 = nn.Linear(2*2*512, 256)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, 10)
         self.relu = nn.ReLU(inplace=True)
@@ -262,11 +304,22 @@ if __name__ == '__main__':
     server.create_socket()
     server.create_db_schema()
     threading.Thread(target=server.listen_for_connections, args=()).start()
-    while len(server.connected_clients) < MIN_PARTICIPANTS:
+    #server.initialize_models()
+    while (len(server.connected_clients) < MIN_PARTICIPANTS) or len(server.connected_clients) != len(server.labeled_clients):
         pass
 
-    for id, (address, sock)  in server.connected_clients.items():
-        print(f'[+] Labeling with client {id, address}')
-        server.send_packet(data={'LABEL_EVENT': b''}, client_socket=sock)
-        server.give_labels(id)
+    for e in range(GLOBAL_EPOCHS):
+        #transmit train signal to each client
+        for client_id, (client_address, client_socket) in server.connected_clients.items():
+            server.send_packet(data={'TRAIN': b''}, client_socket=client_socket)
+        #receive updates from min_clients
+        while len(server.trained_clients) != MIN_PARTICIPANTS:
+            pass
+        server.trained_clients.clear()
+        #aggregate global model
+        #save it o db
+        #transmit global model
+
+    
+
     
